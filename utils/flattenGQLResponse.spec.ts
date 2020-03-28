35715ad62a2a20e8c2330219450ac049f75e5848
Gray Strider <zhoga.ivan@gmail.com>
2020-03-28 14:24:55 +1100

    fix types

--- utils/flattenGQLResponse.spec.ts
+++ utils/flattenGQLResponse.spec.ts
@@ -5,21 +5,21 @@ describe('flattenGQLResponse', () => {
 	it(`should return the input when thre's more than 1 key`, async () => {
 		expect.assertions(1)
 		const input = { foo: 1, bar: 2 }
-		const res: GraphQLResponse = { data: input, status: 200 }
+		const res: any = { data: input, status: 200 }
 		expect(flattenGQLResponse(res)).toStrictEqual(input)
 	})
 
 	it(`should return the value of the first key when there's only one key`, async () => {
 		expect.assertions(1)
 		const input = { foo: 'bar' }
-		const res: GraphQLResponse = { data: input, status: 200 }
+		const res: any = { data: input, status: 200 }
 		expect(flattenGQLResponse(res)).toStrictEqual(input.foo)
 	})
 
 	it('should return errors', async () => {
 		expect.assertions(1)
-		const err: GraphQLError = { message: 'error123' } as GraphQLError
-		const res: GraphQLResponse = { errors: [err], status: 400 }
+		const err: any = { message: 'error123' }
+		const res: any = { errors: [err], status: 400 }
 		expect(() => flattenGQLResponse(res)).toThrow(err.message)
 	})
 })
